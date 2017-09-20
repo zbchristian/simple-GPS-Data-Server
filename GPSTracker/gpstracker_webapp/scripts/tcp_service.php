879defc26e4408ebb42c885f254b8c21bc248151
christian <christian@42cfb47e-d2e2-4637-a3f0-3bfc5f2bbbaf>
2017-09-20 11:56:06 +0000

    Set read timeout for tcp connection in tcp_service.php

--- GPSTracker/gpstracker_webapp/scripts/tcp_service.php
+++ GPSTracker/gpstracker_webapp/scripts/tcp_service.php
@@ -21,12 +21,16 @@ function controlTCPService($action) {
 		$logfile=dirname($TCPBridge)."/log.txt";
 		exec("nohup $TCPBridge >> $logfile 2>&1 &",$out);
 		if(!is_array($out) || (int)$out[0] <= 0) return "Failed to start TCP service";	// check if PID > 0 
-		sleep(2);
+		sleep(2);	// wait for service to start
 		$action = "STATUS";	// check status after start
 	}
+	// create socket and connect to localhost
 	if(($socket=socket_create(AF_INET, SOCK_STREAM, SOL_TCP))=== false || 
-	   ($isConnected = socket_connect($socket, "localhost", $TCPport)) === false && $action==="STATUS") return "TCP service not running/not started";
+	    socket_set_option($socket,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>2, "usec"=>0)) ||
+	   ($isConnected = socket_connect($socket, "localhost", $TCPport)) === false && $action==="STATUS") 
+			return "TCP service not running/not started";
 	if($isConnected === false && $action === "STOP") return true;
+	socket_set_option($socket,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>2, "usec"=>0));	// set timeout for read
 	$cmd= $action === "STATUS" ? "status ".$secretkey : "close ".$secretkey;
 	socket_write($socket, $cmd, strlen($cmd));
 	$response = socket_read($socket,10);
