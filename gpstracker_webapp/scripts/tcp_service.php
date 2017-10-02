9166959710ef37edc8e3ed815255aae239b9121d
christian <christian@42cfb47e-d2e2-4637-a3f0-3bfc5f2bbbaf>
2017-10-02 18:03:10 +0000

    Correct PHP exec command for GO server

--- gpstracker_webapp/scripts/tcp_service.php
+++ gpstracker_webapp/scripts/tcp_service.php
@@ -21,21 +21,20 @@ function controlTCPService($action) {
 	global	$TCPBridge, $TCPport, $HTTPSserver, $secretkey, $urlpath;
 	if($action === "START") {
 		$logfile=dirname($TCPBridge)."/log.txt";
-		if(USE_GO_SERVER) 	exec("nohup $TCPBridge -port $TCPport -httpserver $HTTPSserver -urlpath $urlpath -key $secretkey &> $logfile 2>&1 &");
-		else 				exec("nohup $TCPBridge $TCPport $HTTPSserver $urlpath $secretkey &>> $logfile 2>&1 &");
-//		if(!is_array($out) || (int)$out[0] <= 0) return "Failed to start TCP service";	// check if PID > 0 
+		if(USE_GO_SERVER) 	exec("nohup $TCPBridge -port $TCPport -httpserver $HTTPSserver -urlpath $urlpath -key $secretkey > $logfile &");
+		else 				exec("nohup $TCPBridge $TCPport $HTTPSserver $urlpath $secretkey &> $logfile 2>&1 &");
 		sleep(2);	// wait for service to start
 		$action = "STATUS";	// check status after start
-	}
+	}	
 	// create socket and connect to localhost
 	if(($socket=@socket_create(AF_INET, SOCK_STREAM, SOL_TCP))=== false || 
 	    @socket_set_option($socket,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>2, "usec"=>0)) === false ||
 	   ($isConnected = @socket_connect($socket, "localhost", $TCPport)) === false && $action==="STATUS") 
 			return "TCP service not running/not started";
 	if($isConnected === false && $action === "STOP") return true;
-	$cmd= $action === "STATUS" ? "status ".$secretkey : "close ".$secretkey;
-	@socket_write($socket, $cmd, strlen($cmd));
-	$response = @socket_read($socket,10);
+	$cmd= $action === "STATUS" ? "status ".$secretkey." " : "close ".$secretkey." ";
+	@socket_write($socket, $cmd);
+	$response = @socket_read($socket,20);
 	@socket_close($socket);
 	return $response === "OK";
 }
